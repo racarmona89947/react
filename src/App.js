5f160c5263504e79d90e712a6d12b3d3f6f19908
RAUL ALEJANDRO CARMONA CUELLAR <racarmona89947@umanizales.edu.co>
2022-11-19 01:13:54 -0500

    se carga la imagen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 //import './App.css';
+import logo from './template/assets/img/raul.jpg';
 import './template/css/styles_proyect_raul.css';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
             <a className="navbar-brand js-scroll-trigger" href="#page-top">
                 <span className="d-block d-lg-none">Raul Alejandro Carmona Cuellar</span>
-                <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/raul.jpg" alt="..." /></span>
+                <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={logo} alt="..." /></span>
             </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
